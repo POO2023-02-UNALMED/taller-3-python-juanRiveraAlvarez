c8c4f997a558913c440df367f757379123af358b
juanRiveraAlvarez <juanpariveraalvarez@gmail.com>
2023-08-29 00:54:05 -0700

    5

--- televisores/control.py
+++ televisores/control.py
@@ -16,7 +16,7 @@ class Control:
         self._tv.volumenUp()
 
     def volumenDown(self):
-        self._tv.volumenDown
+        self._tv.volumenDown()
 
     def setCanal(self,canal):
         self._tv.setCanal(canal)
